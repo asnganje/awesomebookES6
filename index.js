88713fa07978937d51f321acbf73f383153f87b2
asnganje <abd.nganje@gmail.com>
2022-08-09 00:33:52 +0300

    modify index.js and fix linter errors

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-import Book from "../modules/bookClass.js";
-import LocalStore from "../modules/localStore.js";
-import FullBook from "../modules/fullBook.js";
-import {DateTime} from "../modules/luxon.js";
+import Book from './modules/bookClass.js';
+import LocalStore from './modules/localStore.js';
+import FullBook from './modules/fullBook.js';
+import { DateTime } from './modules/luxon.js';
 
-document.addEventListener("DOMContentLoaded", FullBook.displayBooks);
+document.addEventListener('DOMContentLoaded', FullBook.displayBooks);
 
-document.querySelector("#booksForm").addEventListener("submit", (e) => {
+document.querySelector('#booksForm').addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
   const book = new Book(title, author);
-    FullBook.addToBooks(book);
-    LocalStore.addBook(book);
-    FullBook.clearField();
+  FullBook.addToBooks(book);
+  LocalStore.addBook(book);
+  FullBook.clearField();
 });
 
-document.querySelector("#ourBooksListId").addEventListener("click", (e) => {
-
-    FullBook.removalBook(e.target);
-    LocalStore.removeBook(e.target.previousElementSibling.textContent);
+document.querySelector('#ourBooksListId').addEventListener('click', (e) => {
+  FullBook.removalBook(e.target);
+  LocalStore.removeBook(e.target.previousElementSibling.textContent);
 });
 
 const dateVisit = document.querySelector('.userDate');
 const setExactTime = () => {
-    const now = DateTime.local();
-    const showDate = now.toFormat("dd LLL yyyy");
-    dateVisit.innerHTML = showDate;
-}
+  const now = DateTime.local();
+  const showDate = now.toFormat('dd LLL yyyy');
+  dateVisit.innerHTML = showDate;
+};
 setExactTime();
 
 const listLink = document.querySelector('.ourBooksList');
@@ -35,25 +34,25 @@ const contactUs = document.querySelector('.contactUs');
 const navigator = document.querySelectorAll('.book-nav');
 
 navigator.forEach((n, index) => n.addEventListener('click', () => {
-    navigator.forEach((link, number) => {
-        if (number === index) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
-    });
-
-    if(index === 0) {
-        listLink.classList.remove('hidden');
-        booksAddition.classList.add('hidden');
-        contactUs.classList.add('hidden');
-    } else if(index == 1){
-        listLink.classList.add('hidden');
-        booksAddition.classList.remove('hidden');
-        contactUs.classList.add('hidden');
+  navigator.forEach((link, number) => {
+    if (number === index) {
+      link.classList.add('active');
     } else {
-        listLink.classList.add('hidden');
-        booksAddition.classList.add('hidden');
-        contactUs.classList.remove('hidden');
+      link.classList.remove('active');
     }
-})); 
+  });
+
+  if (index === 0) {
+    listLink.classList.remove('hidden');
+    booksAddition.classList.add('hidden');
+    contactUs.classList.add('hidden');
+  } else if (index == 1) {
+    listLink.classList.add('hidden');
+    booksAddition.classList.remove('hidden');
+    contactUs.classList.add('hidden');
+  } else {
+    listLink.classList.add('hidden');
+    booksAddition.classList.add('hidden');
+    contactUs.classList.remove('hidden');
+  }
+}));
