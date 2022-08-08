416b166bce2f4035918db2374ed733ca5e6997fb
asnganje <abd.nganje@gmail.com>
2022-08-09 00:09:23 +0300

    add modules, index.html, index.css, index.js and. gitignore files

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# awesomebookES6
+AWESOMEBOOKSES6
+This project is aimed organizing the awesome books application using modules and ES6
+
+Getting Started
+step 1: Clone this repo locally by using the following command
+
+git clone https://github.com/asnganje/awesomebookES6.git
+
+OR, download the entire zip file from github repository.
+
+Step 2: Copy All Project files and run using VSCODE. Open HTML using liveserver to see the User Interface.
+
+Prerequisites
+HTML and css code editor such as VSCode.
+Built With
+HTML 
+CSS
+Javascript
+Modules
+
+See Live here
+
+Author
+👤 Abdulrahman Nganje
+
+GitHub: https://github.com/asnganje
+
+LinkedIn: https://www.linkedin.com/in/abdulrahman-nganje-a6436935/
+
+
+
+🤝 Contributing
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page.
+
+Show your support
+Give a ⭐️ if you like this project!
+
+Acknowledgments
+Miroverse Inc.
---
+++ modules/fullBook.js
@@ -0,0 +1,31 @@
+import LocalStore from "./localStore.js";
+
+export default class FullBook {
+    static displayBooks() {
+        const books = LocalStore.getBooks();
+        books.forEach((book) => FullBook.addToBooks(book));
+    }
+
+    static addToBooks(book) {
+        const bookList = document.getElementById('ourBooksListId');
+        const ListedBooks = document.createElement('div');
+        ListedBooks.classList.add('listed-books');
+        ListedBooks.innerHTML = `
+        <p>${book.title}</p>
+        <p>by</p>
+        <p>${book.author}</p>
+        <button class="remove">Remove</button>`;
+        bookList.appendChild(ListedBooks);
+    }
+
+    static removalBook(element) {
+        if (element.classList.contains('remove')) {
+            element.parentElement.remove();
+        }
+    }
+
+    static clearField() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+      }
+}
